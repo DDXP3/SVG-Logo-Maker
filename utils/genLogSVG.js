4103a3173889861a4f64831d3c6d143fc5334f65
Sichoun N. Lee <Sichounlee769@gmail.com>
2023-11-06 22:12:21 -0600

    got it!

--- utils/genLogSVG.js
+++ utils/genLogSVG.js
@@ -4,13 +4,13 @@ function renShaCol(shapeCol){
 
 function renSha(shape){
     if (shape === "Circle"){
-        return `<circle cx="150" cy="100" r="80" fill="blue"/>`
+        return `circle cx="150" cy="100" r="80"`
     }
     else if (shape === "Triangle"){
-        return `<polygon points="150, 18 244, 182 56, 182" fill="blue" />`
+        return `polygon points="150, 18 244, 182 56, 182"`
     }
     else if (shape === "Square"){
-        return `<square/>`
+        return `rect x="75" y="25" width="150" height="150"`
     }
 }
 
@@ -22,9 +22,9 @@ function genLog(data) {
     return `
     <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-  ${renSha(data.sha)}
+  <${renSha(data.sha)} fill="${renShaCol(data.shaCol)}"/>
 
-  <text x="150" y="125" font-size="60" text-anchor="middle" fill="${renTexCol(data.texCol)}">${data.tex}</text>
+  <text x="150" y="120" font-size="60" text-anchor="middle" fill="${renTexCol(data.texCol)}">${data.tex}</text>
 
 </svg>
     `
